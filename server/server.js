7cfee48da5294eb44f59e864d13162883e530b80
HamzaAkhtar007 <117381318+HamzaAkhtar007@users.noreply.github.com>
2023-10-28 19:34:36 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -44,6 +44,9 @@ app.use(cors({
 app.use(morgan("dev"));
 app.use(express.static('/public'))
 //routes
+app.get("/",(req,res)=>{
+    res.json("Hello");
+})
 app.use('/api/v1/test', testRoutes);
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/user", UserRoutes);
