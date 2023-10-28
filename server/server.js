54ef5052896f78ee54f0469d26b72a305b49612a
HamzaAkhtar007 <117381318+HamzaAkhtar007@users.noreply.github.com>
2023-10-28 19:57:37 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ app.use(ExpressMongoSanitize());
 app.use(xss());
 app.use(json());
 app.use(cors({
-    origin:"",
+    origin:"https://job-shop-client.vercel.app/",
     methods:["POST","GET","PATCH","DELETE","PUT"],
     credentials:true,
 }));
